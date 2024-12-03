0b5cbc617e5b49723b57dd75644e3122b194df71
anshm <anshm@KRISHN>
2024-12-03 17:41:01 -0500

    Add project files.

---
+++ CatchTheStars/Player.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CatchTheStars
+{
+    public class Player
+    {
+        public Vector2 Position { get; private set; }
+        private Texture2D texture;
+
+        private float speed = 300f; // Adjusted player speed for smoother gameplay
+
+        public Player(Texture2D texture, Vector2 initialPosition)
+        {
+            this.texture = texture;
+            this.Position = initialPosition;
+        }
+
+        public void Update(GameTime gameTime, int screenWidth)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            // Move left
+            if (keyboardState.IsKeyDown(Keys.Left) && Position.X > 0)
+            {
+                Position = new Vector2(Position.X - speed * (float)gameTime.ElapsedGameTime.TotalSeconds, Position.Y);
+            }
+
+            // Move right
+            if (keyboardState.IsKeyDown(Keys.Right) && Position.X + texture.Width < screenWidth)
+            {
+                Position = new Vector2(Position.X + speed * (float)gameTime.ElapsedGameTime.TotalSeconds, Position.Y);
+            }
+
+           
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, Color.White);
+        }
+
+        public Rectangle BoundingBox => new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+    }
+}
---
+++ CatchTheStars/Star.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CatchTheStars
+{
+    public class Star
+    {
+        public Vector2 Position { get; private set; }
+        private Texture2D texture;
+
+        private int screenHeight;
+        private float scale = 0.2f; // Scale the star to half its size
+
+        public Star(Texture2D texture, Vector2 initialPosition, int screenHeight = 600)
+        {
+            this.texture = texture;
+            this.Position = initialPosition;
+            this.screenHeight = screenHeight;
+        }
+
+        public void Update(float speed, GameTime gameTime)
+        {
+            // Move the star downward
+            Position = new Vector2(Position.X, Position.Y + speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            // Reset position if the star goes off the bottom of the screen
+            if (Position.Y > screenHeight)
+            {
+                ResetPosition();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the star with the scaling factor
+            spriteBatch.Draw(texture, Position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+
+        public void ResetPosition()
+        {
+            // Reset the star to a random X position and place it back at the top
+            Position = new Vector2(RandomHelper.GetRandomX(), -50);
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, (int)(texture.Width * scale), (int)(texture.Height * scale));
+            }
+        }
+    }
+
+    // A helper class for generating random positions
+    public static class RandomHelper
+    {
+        private static readonly System.Random random = new System.Random();
+
+        public static int GetRandomX(int min = 0, int max = 800) // Screen width default: 800
+        {
+            return random.Next(min, max);
+        }
+    }
+}
