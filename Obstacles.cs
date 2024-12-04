872ae73d146947736cc963a5dc93fb8c9103aaad
GPatidar0022 <168600719+GPatidar0022@users.noreply.github.com>
2024-12-04 01:52:39 -0500

    Add files via upload

---
+++ Obstacles.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace CatchTheStars
+{
+    public class Obstacle
+    {
+        public Vector2 Position { get; private set; }
+        private Texture2D texture;
+
+        private int screenHeight;
+        private static Random random = new Random();
+
+        public Obstacle(Texture2D texture, Vector2 initialPosition, int screenHeight)
+        {
+            this.texture = texture;
+            this.Position = initialPosition;
+            this.screenHeight = screenHeight;
+        }
+
+        public void Update(float speed, GameTime gameTime)
+        {
+            Position = new Vector2(Position.X, Position.Y + speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            if (Position.Y > screenHeight)
+            {
+                ResetPosition();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, Color.White);
+        }
+
+        public void ResetPosition()
+        {
+            Position = new Vector2(random.Next(0, 800), -100); 
+        }
+
+        public Rectangle BoundingBox => new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+    }
+}
